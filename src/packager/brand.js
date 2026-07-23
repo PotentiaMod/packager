9910610f836dab03e30261ac969144685e852722
gaiawindwave90 <crystalfranks18@hotmail.com>
2026-07-23 01:16:19 -0400

    a

--- src/packager/brand.js
+++ src/packager/brand.js
@@ -30,22 +30,22 @@ If this is set to an empty string, the link is removed.
 */
 
 module.exports = {
-  APP_NAME: 'TurboWarp Packager',
-  WEBSITE: 'https://packager.turbowarp.org/',
-  COPYRIGHT_NOTICE: `Copyright (C) 2021-2024 Thomas Weber
+  APP_NAME: 'PotentiaMod Packager',
+  WEBSITE: 'https://potentiamod.github.io/packager/',
+  COPYRIGHT_NOTICE: `Copyright (C) 2026 Crystal Franks
 
 The Source Code Form of this program is available under the terms of the
 Mozilla Public License, v. 2.0. You can obtain a copy of the license at
 https://www.mozilla.org/en-US/MPL/2.0/.`,
-  ACCENT_COLOR: '#ff4c4c',
-  SOURCE_CODE: 'https://github.com/TurboWarp/packager',
+  ACCENT_COLOR: '#4800cc',
+  SOURCE_CODE: 'https://github.com/PotentiaMod/packager',
   FEEDBACK_PRIMARY: {
-    name: 'Scratch',
-    link: 'https://scratch.mit.edu/users/GarboMuffin/#comments'
+    name: 'MistWarp',
+    link: 'https://warp.mistium.com/users/GaiaKitty'
   },
   FEEDBACK_SECONDARY: {
     name: 'GitHub',
-    link: 'https://github.com/TurboWarp/packager/issues'
+    link: 'https://github.com/PotentiaMod/scratch-gui/issues'
   },
   DONATE: '',
   PRIVACY_POLICY: 'https://turbowarp.org/privacy.html',
